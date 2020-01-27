3b9a3f94761ba3325cd536823732f1b386c7c0e6
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-27 12:54:50 +0100

    prepare to connect js file

--- cypress/integration/userCanCreateContact.feature.js
+++ cypress/integration/userCanCreateContact.feature.js
@@ -27,5 +27,12 @@ describe('user can create a contact', () => {
 		cy.get('#twitter').type('@thomasochman')
 		cy.get('#submit').click()
 	})
+	it('displays a name of the new contact', () => {
+		cy.get('#contact-list').should('contain', 'Thomas')
+	})
+	
+	it('displays the phone number of the new contact', () => {
+		cy.get('#contact-list').should('contain', '0700 101010')
+	})
 })
 
