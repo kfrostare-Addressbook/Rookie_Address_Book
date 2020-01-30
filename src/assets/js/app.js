81d50f233663f000d6753a1a2a8c15f13a4a2511
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-30 09:33:31 +0100

    add updateButton code without notes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,24 +4,67 @@ const renderContacts = () => {
   const contacts = JSON.parse(storage.getItem("contacts"));
 
   let div = document.querySelector("#contact-list");
-    if (contacts) {
-      div.innerHTML = "";
-        const ul = document.createElement("ul");
+  if (contacts) {
+    div.innerHTML = "";
+    const ul = document.createElement("ul");
 
-      contacts.forEach(contact => {
-        let li = document.createElement("li");
+    contacts.forEach(contact => {
+      let li = document.createElement("li");
 
-      li.innerHTML = `
-		  <span>${contact.name}</span> |
-		  <span>${contact.email}</span> |
-		  <span>${contact.phone}</span> |
-		  <span>${contact.company}</span> |
-		  <span>${contact.notes}</span> |
+       li.innerHTML = `
+		  <span class="input">${contact.name}</span> |
+		  <span class="input">${contact.email}</span> |
+		  <span class="input">${contact.phone}</span> |
+		  <span class="input">${contact.company}</span> |
+		  <span class="input">${contact.notes}</span> |
       <button id="edit-button" button class="edit-button">Edit</button>
       <button id="remove-button" button class="remove-button" data-email=${contact.email}>Remove</button>
       `;
+     
 
-	let buttons = li.querySelectorAll(".remove-button");
+      let editButtons = li.querySelectorAll(".edit-button");
+      editButtons.forEach(button => {
+        button.addEventListener("click", () => {
+          li.innerHTML = `
+    <form id="contact">
+      <span class="input"><input name="name" type="text" placeholder="name" value=${contact.name}></input></span> |
+      <span class="input"><input name="email" type="text" placeholder="email" value=${contact.email}></input></span>|
+      <span class="input"><input name="phone" type="text" placeholder="phone" value=${contact.phone}></input></span>|
+      <span class="input"><input name="company" type="text" placeholder="company" value=${contact.company}></input></span>|
+      <span class="input"><input name="notes" type="text" placeholder="notes" value=${contact.notes}></input></span> |
+      <input id="submit" type="submit" value="Update">
+      </form>
+
+      `;
+          const updateContact = document.getElementById("contact");
+          updateContact.addEventListener("submit", event => {
+            event.preventDefault();
+
+            const contact = {
+              name: updateContact.elements[0].value,
+              email: updateContact.elements[1].value,
+              phone: updateContact.elements[2].value,
+              company: updateContact.elements[3].value,
+              notes: updateContact.elements[4].value
+            };
+            storage.setItem("contacts", JSON.stringify(contact));
+            console.log(contact);
+          
+            li.innerHTML = `
+            <span class="input">${contact.name}</span> |
+            <span class="input">${contact.email}</span> |
+            <span class="input">${contact.phone}</span> |
+            <span class="input">${contact.company}</span> |
+            <span class="input">${contact.notes}</span> |
+        <button id="edit-button" button class="edit-button">Edit</button>
+        <button id="remove-button" button class="remove-button" data-email=${contact.email}>Remove</button>
+        `;
+        storage.clear()
+          });
+        });
+      });
+
+      let buttons = li.querySelectorAll(".remove-button");
       buttons.forEach(button => {
         button.addEventListener("click", () => {
           var contacts = JSON.parse(localStorage.contacts);
@@ -37,8 +80,8 @@ const renderContacts = () => {
     });
 
     div.appendChild(ul);
-  }   else {
-    div.innerHTML = "<p>You have no contacts in your address book</p>";
+  } else {
+    div.innerHTML = "<p>you have no contacts in your address book </p>";
   }
 };
 
