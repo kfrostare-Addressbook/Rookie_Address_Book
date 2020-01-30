72f06a1fca854256e035c179b019719faafc9f19
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-30 15:56:44 +0100

    commenting out edit button functions

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -23,47 +23,47 @@ const renderContacts = () => {
       `;
      
       // Creates an edit button for each contact in the contact list
-      let editButtons = li.querySelectorAll(".edit-button");
-      editButtons.forEach(button => {
-        button.addEventListener("click", () => {
-          li.innerHTML = `
-    <form id="contact">
-      <span class="input"><input name="name" type="text" placeholder="name" value=${contact.name}></input></span> |
-      <span class="input"><input name="email" type="text" placeholder="email" value=${contact.email}></input></span>|
-      <span class="input"><input name="phone" type="text" placeholder="phone" value=${contact.phone}></input></span>|
-      <span class="input"><input name="company" type="text" placeholder="company" value=${contact.company}></input></span>|
-      <span class="input"><input name="notes" type="text" placeholder="notes" value=${contact.notes}></input></span> |
-      <input id="submit" type="submit" value="Update">
-      </form>
+    //   let editButtons = li.querySelectorAll(".edit-button");
+    //   editButtons.forEach(button => {
+    //     button.addEventListener("click", () => {
+    //       li.innerHTML = `
+    // <form id="contact">
+    //   <span class="input"><input name="name" type="text" placeholder="name" value=${contact.name}></input></span> |
+    //   <span class="input"><input name="email" type="text" placeholder="email" value=${contact.email}></input></span>|
+    //   <span class="input"><input name="phone" type="text" placeholder="phone" value=${contact.phone}></input></span>|
+    //   <span class="input"><input name="company" type="text" placeholder="company" value=${contact.company}></input></span>|
+    //   <span class="input"><input name="notes" type="text" placeholder="notes" value=${contact.notes}></input></span> |
+    //   <input id="submit" type="submit" value="Update">
+    //   </form>
 
       `;  // Assigns a value/id to each added contact
-          const updateContact = document.getElementById("contact");
-          updateContact.addEventListener("submit", event => {
-            event.preventDefault();
+      //     const updateContact = document.getElementById("contact");
+      //     updateContact.addEventListener("submit", event => {
+      //       event.preventDefault();
 
-            const contact = {
-              name: updateContact.elements[0].value,
-              email: updateContact.elements[1].value,
-              phone: updateContact.elements[2].value,
-              company: updateContact.elements[3].value,
-              notes: updateContact.elements[4].value
-            };
-            storage.setItem("contacts", JSON.stringify(contact));
-            console.log(contact);
+      //       const contact = {
+      //         name: updateContact.elements[0].value,
+      //         email: updateContact.elements[1].value,
+      //         phone: updateContact.elements[2].value,
+      //         company: updateContact.elements[3].value,
+      //         notes: updateContact.elements[4].value
+      //       };
+      //       storage.setItem("contacts", JSON.stringify(contact));
+      //       console.log(contact);
           
-            li.innerHTML = `
-            <span class="input">${contact.name}</span> |
-            <span class="input">${contact.email}</span> |
-            <span class="input">${contact.phone}</span> |
-            <span class="input">${contact.company}</span> |
-            <span class="input">${contact.notes}</span> |
-        <button id="edit-button" button class="edit-button">Edit</button>
-        <button id="remove-button" button class="remove-button" data-email=${contact.email}>Remove</button>
-        `;
-        storage.clear()
-          });
-        });
-      });
+      //       li.innerHTML = `
+      //       <span class="input">${contact.name}</span> |
+      //       <span class="input">${contact.email}</span> |
+      //       <span class="input">${contact.phone}</span> |
+      //       <span class="input">${contact.company}</span> |
+      //       <span class="input">${contact.notes}</span> |
+      //   <button id="edit-button" button class="edit-button">Edit</button>
+      //   <button id="remove-button" button class="remove-button" data-email=${contact.email}>Remove</button>
+      //   `;
+      //   storage.clear()
+      //     });
+      //   });
+      // });
 
       // Adds a remove button next to each added contact
       let buttons = li.querySelectorAll(".remove-button");
